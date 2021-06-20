4a1e3dd2b19484b200a9bc232fcbee9ca34d5bec
Joaquin Abrego Perez <joaquin_abrego96@hotmail.com>
2021-06-20 00:55:31 -0300

    Converted PhotoList to functional component

--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -1,23 +1,26 @@
-import React, {Component} from 'react';
+import React, { useState, useEffect} from 'react';
 import {ScrollView, Text, View} from 'react-native';
 import axios from 'axios';
 import PhotoDetail from './PhotoDetail';
 
-class PhotoList extends Component {
-  state = {photos: null};
+const PhotoList = () => {
 
-  componentWillMount() {
-    axios
-      .get(
+  const [photos, setPhotos] = useState(null);
+
+  useEffect(() => {
+    
+    const fetchPhotos = async () =>{
+      const { data } = await axios.get(
         `https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=6e8a597cb502b7b95dbd46a46e25db8d&photoset_id=${this.props.route.params.albumId}&user_id=137290658%40N08&format=json&nojsoncallback=1`,
       )
-      .then((response) =>
-        this.setState({photos: response.data.photoset.photo}),
-      );
-  }
+        setPhotos(data.photoset.photo);
+    }
 
-  renderAlbums() {
-    return this.state.photos.map((photo) => (
+    fetchPhotos();
+  }, []);
+
+  const renderAlbums = () => {
+    return photos.map((photo) => (
       <PhotoDetail
         key={photo.title}
         title={photo.title}
@@ -26,23 +29,22 @@ class PhotoList extends Component {
     ));
   }
 
-  render() {
-    console.log(this.state);
+  console.log(photos);
 
-    if (!this.state.photos) {
-      return (
-        <View style={{flex: 1}}>
-          <Text>Loading...</Text>
-        </View>
-      );
-    }
-
+  if (!photos) {
     return (
       <View style={{flex: 1}}>
-        <ScrollView>{this.renderAlbums()}</ScrollView>
+        <Text>Loading...</Text>
       </View>
     );
   }
+
+  return (
+    <View style={{flex: 1}}>
+      {/* TODO Replace ScrollView with a FlatList */}
+      <ScrollView>{this.renderAlbums()}</ScrollView>
+    </View>
+  );
 }
 
 export default PhotoList;
