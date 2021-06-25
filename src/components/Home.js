8916661685a64fd6a9fa47df969a022b65b7dd9d
Joaquin Abrego Perez <joaquin_abrego96@hotmail.com>
2021-06-25 16:17:57 -0300

    Styled Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,53 @@
 import React from 'react';
-import { View, Button, StyleSheet } from 'react-native';
+import { View, Button, StyleSheet, Image, Text, Insets } from 'react-native';
 
 const styles = StyleSheet.create({
- container:{
-     flex: 1,
+ buttonContainer:{
+     flex: 10,
      flexDirection: 'row',
      alignItems: 'center',
-     justifyContent: 'space-evenly'
+     justifyContent: 'space-around'
+ },
+ container:{
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-evenly'
+ },
+ image:{
+     aspectRatio: 0.5,
+     resizeMode: 'contain',
  }
 });
 
 const Home = ({ navigation }) =>{
-    return(
+    return(      
         <View style={styles.container}>
-            <Button 
-                onPress={()=> navigation.navigate('albumList')}
-                title='Fetch Albums!'
-            >
-            </Button>
-            <Button title="Type n' Search">
-            </Button>
+            <View style={{flex: 2}}>
+                <Image source={require('../../assets/images/logoUTN.png')} style={styles.image}/>
+            </View>
+            <View style={styles.buttonContainer}>
+                <View style={{marginRight: 10}}>
+                    <Button
+                        onPress={()=> navigation.navigate('albumList')}
+                        title='Fetch Albums!'
+                    >
+                    </Button>
+                </View>    
+
+                <View style={{marginLeft:10}}>
+                    <Button title="Type n' Search">
+                    </Button>
+                </View>
+
+            </View>
+            <View style={{flex:2}}>
+                <Text style={{textAlign: 'center'}}>
+                    {'Universidad Tecnológica Nacional\n'}
+                    {'Facultad Regional Córdoba\n'}
+                    {'DSM'}
+                </Text>
+            </View>
         </View>
     );
 };
